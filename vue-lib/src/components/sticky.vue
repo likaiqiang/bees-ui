6ad61f27f9f32ace6923d908024a10d640ef9573
likaiqiang <921085426@qq.com>
2019-01-26 23:10:54 +0800

    调试sticky组件

--- vue-lib/src/components/sticky.vue
+++ vue-lib/src/components/sticky.vue
@@ -1,50 +1,104 @@
 <template>
-    <div ref="stickyWrapper">
-        <div ref="sticky" :style="styles">
-            <slot></slot>
-        </div>
-    </div>   
+  <div :style="wrapperStyles" ref="wrapper">
+    <div :style="styles" ref="sticky">
+      <slot></slot>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
+  data() {
+    return {
+      isSticky: false,
+      wrapperHeight: "auto",
+      width:'auto'
+    };
+  },
+  props: {
+    distance: {
+      type: Number,
+      default: 0
+    }
+  },
+  computed: {
+    styles() {
+      return {
+        position: this.isSticky ? 'fixed' : 'static',
+        top: this.isSticky ? this.distance + 'px' : "",
+        width:this.width
+      };
+    },
+    wrapperStyles(){
         return {
-            sticky:false
-        }
-    },
-    methods:{
-        getElementHeight(){
-            return this.$refs.stickyWrapper.getBoundingClientRect.height
-        },
-        getElementTop(){
-            return this.$refs.stickyWrapper.getBoundingClientRect.top
-        }
-    },
-    mounted(){
-        var top = window.pageY + this.getElementTop()
-        window.addEventListener('scroll',()=>{
-            if(window.pageY > top){
-                this.$refs.stickyWrapper.style.height = `${this.getElementHeight()}px`
-                this.sticky = true
-            }
-            else{
-                this.$refs.stickyWrapper.style.height = ''
-                this.sticky = false
-            }
-        })
-    },
-    computed:{
-        styles(){
-           return this.sticky ? {
-               position:'fixed',
-               width:'100%'
-           } : {}
+            height:this.wrapperHeight
         }
     }
-}
+  },
+  mounted() {
+    this.width = this.$refs.sticky.getBoundingClientRect().width + 'px'
+
+    window.addEventListener('resize',()=>{
+        this.width = document.body.getBoundingClientRect().width + 'px'
+    })
+    window.addEventListener("scroll", () => {
+      var rectTop = this.$refs.wrapper.getBoundingClientRect().top;
+      if (rectTop < this.distance) {
+        this.isSticky = true;
+        console.log('滚过了')
+      } else {
+        this.isSticky = false;
+        console.log('没有滚过')
+      }
+    });
+    if (window.MutationObserver) {
+      var mutationObserver = new window.MutationObserver(function(mutations) {
+          console.log('mutations')
+        if (
+          mutations[0].addedNodes.length ||
+          mutations[0].removedNodes.length
+        ) {
+          this.updateHeight()
+        }
+      });
+      mutationObserver.observe(this.$refs.sticky, {
+        subtree: true,
+        childList: true
+      });
+    } else {
+      if (window.addEventListener) {
+        this.$refs.sticky.addEventListener("DOMNodeInserted",()=>{
+            this.updateHeight()
+        });
+        this.$refs.sticky.addEventListener("DOMNodeRemoved",()=>{
+            this.updateHeight()
+        });
+      } else if (window.attachEvent) {
+        this.$refs.sticky.attachEvent("onDOMNodeInserted", ()=>{
+          this.updateHeight()
+        });
+        this.$refs.sticky.attachEvent("onDOMNodeRemoved", ()=>{
+          this.updateHeight()
+        });
+      }
+    }
+  },
+  watch: {
+    isSticky: function(newVal) {
+      if (newVal) {
+        this.$emit('sticky-start')
+        this.updateHeight()
+      }
+      else{
+          this.$emit('sticky-end')
+      }
+    }
+  },
+  methods:{
+      updateHeight(){
+          this.wrapperHeight = getComputedStyle(this.$refs.sticky).height;
+          this.$emit('sticky-update')
+      }
+  }
+};
 </script>
-
-<style lang="scss">
-
-</style>
