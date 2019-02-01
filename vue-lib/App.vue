4e104457ef2e7e7f86e31e1a09944d977161d7b4
likaiqiang <921085426@qq.com>
2019-02-01 16:34:08 +0800

    完善input组件

--- vue-lib/App.vue
+++ vue-lib/App.vue
@@ -1,8 +1,10 @@
 <template>
   <div id="app">
     <ui-button @click="loading = !loading" :loading="loading" type="warning">button</ui-button>
-    <ui-input v-model="value"></ui-input>
-    <ui-icon type="md-heart-outline" color="red"/>
+    <ui-input v-model="value" :close="true"></ui-input>
+    <ui-input width="450px" v-model="value" :close="true">
+      <ui-button slot="append" type="primary">search</ui-button>
+    </ui-input>
   </div>
 </template>
 
@@ -10,7 +12,6 @@
 import Button from '@/components/button/index.js'
 import Input from '@/components/input/index.js'
 import Icon from '@/components/icon/index.js'
-console.log(Button)
 
 export default {
   name: 'app',
--- vue-lib/src/components/input/src/input.vue
+++ vue-lib/src/components/input/src/input.vue
@@ -1,41 +1,60 @@
 <template>
-  <div :class="classes" :style="styles" :readonly="readonly" :disabled="disabled">
-    <input type="text" v-model="val">
+  <div :class="classes">
+    <div class="ui-input-wrapper" @mouseenter="isHover = true"
+        @mouseleave="isHover = false">
+      <div class="ui-input-prepend">
+        <slot name="prepend"></slot>
+      </div>
+      <input
+        ref="input"
+        :type="type"
+        :style="inputStyles"
+        @focus="focusHandler"
+        @blur="blurHandler"
+        :placeholder="placeholder"
+        v-model="val"
+        :readonly="readonly"
+        :disabled="disabled"
+        :autocomplete="autocomplete"
+        :autofocus="autofocus"
+      >
+      <ui-icon v-show="close && isHover" @click="closeHandler" type="ios-close-circle"/>
+    </div>
+    <div class="ui-input-append">
+      <slot name="append"></slot>
+    </div>
   </div>
 </template>
 <script>
+import "@/components/icon/index.js";
 export default {
-  name:'ui-input',
-  props:{
-    value:{
-      required:true,
-      type:String
+  name: "ui-input",
+  data(){
+    return {
+      isHover:false
     }
   },
   computed: {
     classes() {
       return ["ui-input"];
     },
-    styles() {
+    inputStyles(){
       return {
-        display: "inline-block",
-        width: this.block ? "100%" : "auto"
-      };
+        borderRadius:this.radius,
+        width:this.width
+      }
     },
-    val:{
-      get(){
-        return this.value
+    val: {
+      get() {
+        return this.value;
       },
-      set(val){
-        this.$emit('input',val)
+      set(val) {
+        this.$emit("input", val);
+        this.$emit("change", val);
       }
     }
   },
   props: {
-    block: {
-      type: Boolean,
-      default: false
-    },
     readonly: {
       type: Boolean,
       default: false
@@ -44,8 +63,53 @@ export default {
       type: Boolean,
       default: false
     },
-    count:{
-        type:Number
+    value: {
+      required: true,
+      type: String
+    },
+    close: {
+      default: false,
+      type: Boolean
+    },
+    placeholder: {
+      type: String,
+      default: ""
+    },
+    type: {
+      type: String,
+      default: "text"
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    },
+    autocomplete: {
+      type: String,
+      default: "off"
+    },
+    radius:{
+      type:String,
+      default:'0px'
+    },
+    width:{
+      type:String,
+      default:'auto'
+    }
+  },
+  methods: {
+    closeHandler() {
+      this.val = "";
+    },
+    focusHandler() {
+      this.$emit("focus");
+    },
+    blurHandler() {
+      this.$emit("blur");
+    }
+  },
+  mounted() {
+    if (this.autofocus) {
+      this.$refs.input.focus();
     }
   }
 };
