d83e4f917eeb483da148072a307e6058299d5468
likaiqiang <921085426@qq.com>
2019-04-04 19:18:15 +0800

    完成circle组件

--- vue-lib/src/components/circle/src/circle.vue
+++ vue-lib/src/components/circle/src/circle.vue
@@ -1,10 +1,14 @@
 <template>
-  <div class="dib">
-    <svg :width="width" :height="height" viewbox="0 0 440 440">
-      <circle cx="220" cy="220" r="170" stroke-width="50" stroke="#D1D3D7" fill="none"></circle>
-      <circle cx="220" cy="220" r="170" stroke-width="50" stroke="#00A5E0" fill="none" transform="matrix(0,-1,1,0,0,440)" :stroke-dasharray="value+' '+1069"></circle>
-    </svg>
-  </div>
+
+    <div class="ui-circle">
+      <svg :width="width" :height="height" viewBox="0 0 440 440">
+        <circle cx="220" cy="220" r="170" stroke-width="50" stroke="#D1D3D7" fill="none"></circle>
+        <circle cx="220" cy="220" r="170" stroke-width="50" stroke="#00A5E0" fill="none" transform="matrix(0,-1,1,0,0,440)" :stroke-dasharray="value+' '+1069"></circle>
+      </svg>
+      <div class="ui-circle-content" :style="{width:width/2+'px',height:height/2+'px'}">
+        <slot></slot>
+      </div>
+    </div>
 </template>
 
 <script>
@@ -16,12 +20,12 @@ export default {
       default:0
     },
     width:{
-      type:[String,Number],
-      default:440
+      type:[Number],
+      default:100
     },
     height:{
-      type:[String,Number],
-      default:440
+      type:[Number],
+      default:100
     }
   }
 }
