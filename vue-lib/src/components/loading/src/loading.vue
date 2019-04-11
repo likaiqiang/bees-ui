3a28a87b6b032be200a7bbe62d910f85fc6e34a9
likaiqiang <921085426@qq.com>
2019-04-11 16:39:17 +0800

    loading添加value

--- vue-lib/src/components/loading/src/loading.vue
+++ vue-lib/src/components/loading/src/loading.vue
@@ -18,6 +18,10 @@ export default {
       primary:{
         type:Boolean,
         default:false
+      },
+      value:{
+        type:Boolean,
+        default:false
       }
     },
     computed:{
